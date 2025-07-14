e86fcbf578657f71ed93e3fe6becf290ecc5a14d
edgewoodhsrobotics <edgewoodhighschoolrobotics@gmail.com>
2025-07-14 11:50:38 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,7 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+
 package frc.robot;
+
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -11,23 +13,31 @@ import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class Robot extends TimedRobot {
   SparkMax leftLeader;
   SparkMax leftFollower;
   SparkMax rightLeader;
   SparkMax rightFollower;
+  SparkMax elevatorLeader;
+  SparkMax elevatorFollower;
   XboxController joystick;
+
 
   public Robot() {
     // Initialize the SPARKs
     leftLeader = new SparkMax(4, MotorType.kBrushed);
     leftFollower = new SparkMax(2, MotorType.kBrushed);
     rightLeader = new SparkMax(3, MotorType.kBrushed);
-    rightFollower = new SparkMax(1,MotorType.kBrushed);
+    rightFollower = new SparkMax(1, MotorType.kBrushed);
+    elevatorLeader = new SparkMax(5, MotorType.kBrushless);
+    elevatorFollower = new SparkMax(6, MotorType.kBrushless);
+
 
     /*
      * Create new SPARK MAX configuration objects. These will store the
@@ -37,6 +47,9 @@ public class Robot extends TimedRobot {
     SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
     SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
     SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig elevatorLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig elevatorFollowerConfig = new SparkMaxConfig();
+
 
     /*
      * Set parameters that will apply to all SPARKs. We will also use this as
@@ -46,20 +59,30 @@ public class Robot extends TimedRobot {
         .smartCurrentLimit(50)
         .idleMode(IdleMode.kBrake);
 
+
     // Apply the global config and invert since it is on the opposite side
     rightLeaderConfig
         .apply(globalConfig)
         .inverted(true);
+
 
     // Apply the global config and set the leader SPARK for follower mode
     leftFollowerConfig
         .apply(globalConfig)
         .follow(leftLeader);
 
+
     // Apply the global config and set the leader SPARK for follower mode
     rightFollowerConfig
         .apply(globalConfig)
         .follow(rightLeader);
+
+    elevatorLeaderConfig.apply(globalConfig);
+
+    elevatorFollowerConfig 
+    .apply(globalConfig)
+    .follow(elevatorLeader);
+
 
     /*
      * Apply the configuration to the SPARKs.
@@ -75,10 +98,13 @@ public class Robot extends TimedRobot {
     leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    elevatorLeader.configure(elevatorLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    elevatorFollower.configure(elevatorFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
     // Initialize joystick
     joystick = new XboxController(0);
   }
+
 
   @Override
   public void robotPeriodic() {
@@ -87,54 +113,76 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
   }
 
+
   @Override
   public void autonomousInit() {
   }
+
 
   @Override
   public void autonomousPeriodic() {
   }
 
+
   @Override
   public void teleopInit() {
   }
 
+
   @Override
   public void teleopPeriodic() {
-    /**
-     * Get forward and rotation values from the joystick. Invert the joystick's
-     * Y value because its forward direction is negative.
-     */
+   
+  //Tank Drive
+  /*
+  double leftPower = joystick.getLeftY();
+  double rightPower = joystick.getRightY();
+
+  leftLeader.set(Math.pow(leftPower, 3));
+  rightLeader.set(Math.pow(rightPower, 3));
+  
+   */
+
+    //Arcade Drive
     double forward = Math.pow(joystick.getLeftY(), 3);
     double rotation = Math.pow(joystick.getRightX(), 3);
 
-    /*
-     * Apply values to left and right side. We will only need to set the leaders
-     * since the other motors are in follower mode.
-     */
     leftLeader.set(forward - rotation);
-    rightLeader.set(forward +rotation);
+   
+    rightLeader.set(forward + rotation);
+
+    //Elevator
+
+    double rightTrigger = joystick.getRightTriggerAxis();
+    double leftTrigger = joystick.getLeftTriggerAxis();
+    double elevatorPower = rightTrigger - leftTrigger;
+    elevatorLeader.set(elevatorPower);
   }
+
 
   @Override
   public void disabledInit() {
   }
 
+
   @Override
   public void disabledPeriodic() {
   }
+
 
   @Override
   public void testInit() {
   }
 
+
   @Override
   public void testPeriodic() {
   }
 
+
   @Override
   public void simulationInit() {
   }
+
 
   @Override
   public void simulationPeriodic() {
