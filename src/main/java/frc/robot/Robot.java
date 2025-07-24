d820eb794c167d98463850de92b0e84dd924dd0d
EdgewoodHsRobotics <edgewoodhighschoolrobotics@gmail.com>
2025-07-24 10:40:33 -0500

    Added autonomous

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,12 +12,13 @@ import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-
-
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 
 public class Robot extends TimedRobot {
   SparkMax leftLeader;
@@ -27,7 +28,8 @@ public class Robot extends TimedRobot {
   SparkMax elevatorLeader;
   SparkMax elevatorFollower;
   XboxController joystick;
-
+  Solenoid latchSolenoid;
+  Timer autoTimer;
 
   public Robot() {
     // Initialize the SPARKs
@@ -37,6 +39,8 @@ public class Robot extends TimedRobot {
     rightFollower = new SparkMax(1, MotorType.kBrushed);
     elevatorLeader = new SparkMax(5, MotorType.kBrushless);
     elevatorFollower = new SparkMax(6, MotorType.kBrushless);
+    latchSolenoid = new Solenoid(PneumaticsModuleType.REVPH, 0);
+    autoTimer = new Timer();
 
 
     /*
@@ -49,8 +53,7 @@ public class Robot extends TimedRobot {
     SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
     SparkMaxConfig elevatorLeaderConfig = new SparkMaxConfig();
     SparkMaxConfig elevatorFollowerConfig = new SparkMaxConfig();
-
-
+    
     /*
      * Set parameters that will apply to all SPARKs. We will also use this as
      * the left leader config.
@@ -59,30 +62,29 @@ public class Robot extends TimedRobot {
         .smartCurrentLimit(50)
         .idleMode(IdleMode.kBrake);
 
-
     // Apply the global config and invert since it is on the opposite side
     rightLeaderConfig
         .apply(globalConfig)
         .inverted(true);
-
 
     // Apply the global config and set the leader SPARK for follower mode
     leftFollowerConfig
         .apply(globalConfig)
         .follow(leftLeader);
 
-
-    // Apply the global config and set the leader SPARK for follower mode
+    // Apply the global config
     rightFollowerConfig
         .apply(globalConfig)
         .follow(rightLeader);
 
-    elevatorLeaderConfig.apply(globalConfig);
+    // Apply the global config and set the leader SPARK for follower mode
+
+    elevatorLeaderConfig
+    .apply(globalConfig);
 
     elevatorFollowerConfig 
     .apply(globalConfig)
     .follow(elevatorLeader);
-
 
     /*
      * Apply the configuration to the SPARKs.
@@ -105,7 +107,6 @@ public class Robot extends TimedRobot {
     joystick = new XboxController(0);
   }
 
-
   @Override
   public void robotPeriodic() {
     // Display the applied output of the left and right side onto the dashboard
@@ -113,14 +114,32 @@ public class Robot extends TimedRobot {
     SmartDashboard.putNumber("Right Out", rightLeader.getAppliedOutput());
   }
 
-
   @Override
   public void autonomousInit() {
+    autoTimer.reset();
+    autoTimer.start();
   }
 
 
   @Override
   public void autonomousPeriodic() {
+    double time = autoTimer.get();
+  
+if (time < 5.5) {
+      leftLeader.set(0.3);
+      rightLeader.set(0.3);
+    } else if (time < 6.2) {
+    leftLeader.set(0.0);
+    rightLeader.set(-0.5);
+    }
+    else if (time < 8.5) {
+      leftLeader.set(0.3);
+      rightLeader.set(0.3);
+      } else {
+      leftLeader.set(0);
+      rightLeader.set(0);
+    }
+  
   }
 
 
@@ -133,24 +152,24 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
    
   //Tank Drive
-  
+  /*
   double leftPower = joystick.getLeftY();
   double rightPower = joystick.getRightY();
 
   leftLeader.set(((Math.pow(leftPower, 3))*0.9)*0.5);
   rightLeader.set(((Math.pow(rightPower, 3))*0.5)*0.5);
-  
+   */
   
 
     //Arcade Drive
-     /*
-    double forward = Math.pow(joystick.getLeftY(), 3);
-    double rotation = Math.pow(joystick.getRightX(), 3);
+    
+    double forward = (Math.pow(joystick.getLeftY(), 3))*0.30;
+    double rotation = (Math.pow(joystick.getRightX(), 3))*0.30;
 
     leftLeader.set(forward - rotation);
    
     rightLeader.set((forward + rotation));
-    */
+    
 
     //Elevator
 
@@ -158,6 +177,14 @@ public class Robot extends TimedRobot {
     double leftTrigger = joystick.getLeftTriggerAxis();
     double elevatorPower = ((Math.pow(((rightTrigger - leftTrigger)), 3)) * 0.5);
     elevatorLeader.set(elevatorPower);
+
+    //Switch
+    if (joystick.getAButton()) {
+      latchSolenoid.set(true);  // Unlock
+  } else {
+      latchSolenoid.set(false); // Lock
+  }
+  
   }
 
 
